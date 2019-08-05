ee982f290a2af629687676976c4b27ed586fbc00
Robert Hingstrum <rhingstrumjr@gmail.com>
2019-08-05 17:56:40 -0500

    Checking out markdown for blog

---
+++ layouts/blog.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <Navbar />
+    <b-container>
+      <b-row align-h="center" class="justify-content-sm-center">
+        <b-col sm="12" class="text-center">
+          <h1>Special blog article view</h1>
+          <h2>Might want to have a sidebar</h2>
+          <p>Or, include links to other posts.</p>
+        </b-col>
+        <nuxt />
+      </b-row>
+    </b-container>
+  </div>
+</template>
+
+<script>
+import Navbar from '@/components/Navbar.vue'
+export default {
+  components: {
+    Navbar
+  }
+}
+</script>
---
+++ pages/posts/learning-vue.vue
@@ -0,0 +1,20 @@
+<template lang="md">
+  # Learning Vue!
+
+  I'm falling in love with Vuejs.  I find using it incredibly simple!
+  <div>
+    <nuxt-link to="/">
+    Back home
+    </nuxt-link>
+  </div>
+</template>
+
+<script>
+import Navbar from '@/components/Navbar.vue'
+export default {
+  layout: 'blog',
+  components: {
+    Navbar
+  }
+}
+</script>
