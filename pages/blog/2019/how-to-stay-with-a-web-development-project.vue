9f34c4675bb4af6f7ea6e7f0cb13dc6c558dd1f4
Robert Hingstrum <rhingstrumjr@gmail.com>
2019-08-14 10:56:39 -0500

    Correct typos

--- pages/blog/2019/how-to-stay-with-a-web-development-project.vue
+++ pages/blog/2019/how-to-stay-with-a-web-development-project.vue
@@ -4,24 +4,24 @@
 
    I consistently run into the feeling of confusion.  It's like I hit the brakes without meaning to.  This is a forced pause.
 
-   I want to explore this forced-pause area.  First, I want to describe what the forced-pause area is.
+   I want to explore this *forced pause*.  First, I want to describe what the *forced pause* is.
 
-   The forced-pause area is when your brain no longer knows the steps to take.  Usually, this is immediately following your great idea.  It is signified by the sound, "Uhh."
+   The *forced-pause* is when your brain no longer knows the steps to take.  Usually, this is immediately following your great idea.  It is signified by the sound, **"Uhh."**
 
-   "Let's throw a party!" "Awesome idea!  Where?"  "Uhh..."
+   "Let's throw a party!" "Awesome idea!  Where?"  **"Uhh..."**
 
-   "This drawer is always a mess.  Let's organize it."  "Awesome idea!  How?"  "Uhh..."
+   "This drawer is always a mess.  Let's organize it."  "Awesome idea!  How?"  **"Uhh..."**
 
-   "Wouldn't it be great if an app helped people do..."  "Awesome idea! How will you make it?  "Uhh..."
+   "Wouldn't it be great if an app helped people do..."  "Awesome idea! How will you make it?  **"Uhh..."**
 
-  ### What do you do when you get caught in the forced-pause area?
+  ### What do you do when you get caught in the forced-pause?
   <UserAnswer v-bind:q-number="1" />
 
-  The forced-paused area is really an invitation to be creative.  Some people are scared of this invitation.  There isn't a defined, correct answer.  We must invent one!
+  The forced pause is really an invitation to be creative.  Some people are scared of this invitation.  There isn't a defined, correct answer.  We must invent one!
 
   Sometimes our inventions work.  That is great!  But, sometimes our inventions fail.  That is a bummer.  It is in this space where a growth mindset is needed.  Turn your failures into learning.  Just don't give up!  Create another invention.
 
-  Here are some questions to help guide you when you get caught in the forced-pause area.
+  Here are some questions to help guide you when you get caught in the forced pause.
   - Why do I want to create this project?
   - What will the project look like?
     - What does a drawing of it look like?
