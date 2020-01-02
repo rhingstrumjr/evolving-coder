b220519d968d1447003295211d9086379ecf91ca
rhingstrumjr <rhingstrumjr@gmail.com>
2020-01-02 09:25:13 -0600

    Plan: Creating todo list from the plan

---
+++ src/components/planning-tool/PlanningToolTodo.vue
@@ -0,0 +1,29 @@
+// Different types of to-dos: // 1. Research // 2. Get // 3. Create // Todos
+will fall into 2 categories: // 1. Actionable - can be done immediately // 2.
+Dependent - must be done after another task
+
+<template> </template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
+
+<template>
+  1.
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
+
+<template> </template>
+
+<script>
+export default {};
+</script>
+
+<style scoped></style>
